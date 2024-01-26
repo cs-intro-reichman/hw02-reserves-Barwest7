5c9df9d912b742709b8cac627c2298279e546fe4
Bar Westfried <bar.westfried@post.runi.ac.il>
2024-01-27 00:29:22 +0200

    Fixed some issues and added the last file

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,13 +4,14 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		int userInput = Integer.parseInt(args[0]);
-		
-		for (int i=0; i<userInput; i++) {
+		for (int i=1; i<=userInput; i++) {
 			if (i%2==0)
 				System.out.print(" ");
 			for (int j=0; j<userInput; j++) {
 				System.out.print("* ");
 			}
+			if (i%2==1)
+				System.out.print(" ");
 			System.out.println();
 		}
 
--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -24,6 +24,69 @@ public class OneOfEachStats {
 		//// just like you had in the previous version, except that the 
 		//// randomization will be based on the given seed.
 		//// This is the only change that you have to do in the program.
+
+		int T = Integer.parseInt(args[0]);
+		int counterOfChildren = 0;
+		int randomNum = -1;
+		char bOrG = 'a';
+		boolean girlFlag = false;
+		boolean boyFlag = false;
+
+		double totalNumOfChildren = 0;
+		int familiesOfTwo = 0;
+		int familiesOfThree = 0;
+		int familiesOfFour = 0;
+		double average;
+		String mostCommon = "";
+
+		// Run for as many times as the user requested
+		for (int i=0; i<T; i++) {
+
+					// Run as long as youd ont have at least 1 of each gender
+			while (!boyFlag || !girlFlag) {
+			
+				// Generate a randoum number: 0 or 1 (50% chance for each)
+				randomNum = (int)generator.nextDouble();
+
+				// Decide tentatively that 0 is for girl and 1 is for boy
+				if (randomNum==1) {
+					bOrG = 'b';
+					boyFlag = true;
+				}
+				else if (randomNum==0) {
+					bOrG = 'g';
+					girlFlag = true;
+				}
+
+			counterOfChildren++;
+			totalNumOfChildren++;
+			}
+
+			if (counterOfChildren == 2)
+				familiesOfTwo++;
+			else if (counterOfChildren == 3)
+				familiesOfThree++;
+			else
+				familiesOfFour++;
+			girlFlag = false;
+			boyFlag = false;
+			counterOfChildren = 0;
+		}
+
+		average = totalNumOfChildren/T;
+
+		if (familiesOfFour > familiesOfThree && familiesOfFour > familiesOfTwo)
+			mostCommon = " 4 or more.";
+		else if (familiesOfThree > familiesOfFour && familiesOfThree > familiesOfTwo)
+			mostCommon = "3.";
+		else 
+			mostCommon = "2.";
+
+		System.out.println("Average: " + average + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + familiesOfTwo);
+		System.out.println("Number of families with 3 children: " + familiesOfThree);
+		System.out.println("Number of families with 4 or more children: " + familiesOfFour);
+		System.out.println("The most common number of children is " + mostCommon);
 		    
 	}
 }
